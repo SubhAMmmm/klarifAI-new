764a0836c255a94f8d74deb655534bfe92f1f227
Subham Sahu <subham.sahu@brand-scapes.com>
2025-01-08 07:51:52 +0530

    changed requirements.txt file

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -5,6 +5,7 @@ from .settings import BASE_DIR
 # Load production environment variables
 DEBUG = True
 
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 SECRET_KEY = os.environ['MY_SECRET_KEY']
